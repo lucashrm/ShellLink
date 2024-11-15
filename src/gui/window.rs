efd1e2e418da068bd833cfd3d95c6d226e1de315
Lucas <lucas.herman@epitech.eu>
2024-11-15 14:37:48 +0100

    ➕ Adding new window with button connect

--- src/gui/window.rs
+++ src/gui/window.rs
@@ -1,3 +1,40 @@
 pub mod screen {
+    use druid::{AppLauncher, Data, Lens, Widget, WidgetExt, WindowDesc};
+    use druid::piet::TextStorage;
+    use druid::widget::{Button, Flex, TextBox};
+    use crate::connexion::client::client::TcpConnexion;
+    use super::super::super::connexion::client::client;
 
+    #[derive(Clone, Data, Lens)]
+    pub struct ClientData {
+        name: String
+    }
+
+    fn ui_builder() -> impl Widget<ClientData> {
+        let mut col = Flex::column();
+        col.add_child(TextBox::new().lens(ClientData::name).padding(10.0));
+
+        let connect_button = Button::new("Connect".to_string())
+            .padding(3.0)
+            .on_click(|_ctx, data: &mut ClientData, _env| {
+                let mut client = TcpConnexion::new("localhost:5444".to_string()).unwrap();
+                client.send_message("Hello".as_str());
+            });
+
+        col.add_child(connect_button);
+
+        Flex::column().with_child(col)
+    }
+
+    pub fn new_window() {
+        let main_window = WindowDesc::new(ui_builder());
+        let data = ClientData {
+            name: "Lulu".to_string()
+        };
+        AppLauncher::with_window(main_window)
+            .log_to_console()
+            .launch(data)
+            .expect("Couldn't launch window");
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use shell_link::config::{Config, ConnexionMode};
 
 use shell_link::connexion::client::client;
 use shell_link::connexion::server::server;
-use shell_link::gui::window;
+use shell_link::gui::window::screen;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,23 +16,24 @@ fn main() {
 
     match setup.get_mode() {
         ConnexionMode::Client => {
-            let mut client = client::TcpConnexion::new("localhost:5444".to_string())
-                .unwrap_or_else(|e| {
-                    eprintln!("Client failed: {}", e);
-                    exit(1);
-                });
-            let msg = "hello";
-            println!("Sending hello");
-
-            client.send_message(msg);
-            loop {
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read line");
-
-                client.send_message(input.as_str());
-            }
+            screen::new_window();
+            // let mut client = client::TcpConnexion::new("localhost:5444".to_string())
+            //     .unwrap_or_else(|e| {
+            //         eprintln!("Client failed: {}", e);
+            //         exit(1);
+            //     });
+            // let msg = "hello";
+            // println!("Sending hello");
+            //
+            // client.send_message(msg);
+            // loop {
+            //     let mut input = String::new();
+            //     io::stdin()
+            //         .read_line(&mut input)
+            //         .expect("Failed to read line");
+            //
+            //     client.send_message(input.as_str());
+            // }
         },
         ConnexionMode::Server => {
             let mut server = server::TcpConnexion::new("0.0.0.0:5444".to_string())
